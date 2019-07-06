07cab87c0d682ccb5aab58be1bbefd54b7d1a72a
src-kun <9331031@qq.com>
2019-07-06 15:27:35 +0800

    Update check.py

--- check.py
+++ check.py
@@ -2,19 +2,26 @@
 
 import sys
 import socket
+import thread
+import time
+
+ports = []
 
 def scan_port(port_num, host):
     s = socket.socket()
-    socket.setdefaulttimeout(2)
+    socket.setdefaulttimeout(3)
     try:
-        s = s.connect((host, port_num))
-        print port_num, "[+] connection successful"
-
+        s.connect((host, port_num))
+        ports.append(port_num)
     except Exception, e:
         pass
 
 #host = 'localhost'
-host =sys.argv[1]
+host = sys.argv[1]
 
-for i in xrange(65535):
-    scan_port(i, host)
+for i in [22,8000,8080,3306,21,80,443]:
+    try:
+        scan_port(i, host)
+    except:
+        pass
+print ports
